adf5098fa1ab254ac66ca4904347cbf00b7ea41b
CodeBreaker03 <vijaykrishnanceg@gmail.com>
2016-06-16 16:16:53 +0530

    Initial commit

---
+++ app/src/main/java/bazzinga/aboutus/MyData.java
@@ -0,0 +1,16 @@
+package bazzinga.aboutus;
+
+/**
+ * Created by VJ on 16-Jun-16.
+ */
+
+
+public class MyData {
+
+    static String[] nameArray = {"VIJAY KRISHNAN", "AMITH AJITH"};
+    static String[] versionArray = {"9841698474", "9176185122"};
+
+    static Integer[] drawableArray = {R.drawable.vj, R.drawable.am};
+
+    static Integer[] id_ = {0, 1};
+}
